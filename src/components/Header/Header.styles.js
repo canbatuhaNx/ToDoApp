e6469c00354bd87815f0b4e772051ec56b61c2db
Can Batuhan <icanbatuhan@gmail.com>
2023-03-26 22:40:44 +0300

    ToDoApp
    Yapılacaklar (Hatırlatıcı) Uygulaması

---
+++ src/components/Header/Header.styles.js
@@ -0,0 +1,18 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container:{
+      flexDirection:'row',
+      padding:10,
+    },
+    intxt:{
+      flex:1,
+      fontSize:30,
+      fontWeight:'bold',
+      color:'#ffa500',
+    },
+    counter:{
+      fontSize:30,
+      color:'#ffa500',
+    },
+});
